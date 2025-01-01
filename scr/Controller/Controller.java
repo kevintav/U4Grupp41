0e73c0aa23cca41c5d74c23286fa27f40cfc6411
Bjornheimer89 <christoffer.bjornheimer@gmail.com>
2025-01-01 23:48:32 +0100

    testet

--- scr/Controller/Controller.java
+++ scr/Controller/Controller.java
@@ -1,11 +1,13 @@
 package Controller;
+
 import View.MainFrame;
 
 public class Controller {
     private MainFrame view;
-    public Controller(){
 
-        int sizeOfBoard=10;
+    public Controller() {
+
+        int sizeOfBoard = 5; //Kan inte sättas högre än 20, men det går om man tar bort specialfärgen
         view = new MainFrame(800, 800, this, sizeOfBoard);
 
     }
@@ -13,8 +15,6 @@ public class Controller {
     public static void getClickedButton(int index) {
         System.out.println("tryckte på " + index);
     }
-
-
 
 
 }
