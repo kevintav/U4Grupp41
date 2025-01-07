a57c721fc3dc3338f36ce519ea1f05c36914b332
Kevin Taverner <Kevin@amanzi.com>
2025-01-07 17:29:06 +0100

    Correct code structure

--- scr/Controller/Controller.java
+++ scr/Controller/Controller.java
@@ -6,17 +6,11 @@ public class Controller {
     private MainFrame view;
 
     public Controller() {
-
         int sizeOfBoard = 10; //Kan inte sättas högre än 20, men det går om man tar bort specialfärgen
         view = new MainFrame(800, 800, this, sizeOfBoard);
-
     }
 
     public static void getClickedButton(int index) {
         System.out.println("tryckte på " + index);
     }
-
-
-
-
 }
