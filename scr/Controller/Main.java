61a7e030277622661c640b4e507ae863350f74d3
Bjornheimer89 <christoffer.bjornheimer@gmail.com>
2025-01-10 14:42:01 +0100

    testet

--- scr/Controller/Main.java
+++ scr/Controller/Main.java
@@ -5,22 +5,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*JFrame test = new JFrame();
-        JLabel hiscore = new JLabel("STEFAN",SwingConstants.CENTER);
-        test.setSize(400,500);
-        test.setLocationRelativeTo(null);
-        test.setResizable(false);
-        test.setVisible(true);
-        test.setBackground(Color.WHITE);
-        hiscore.setBackground(Color.black);
-        hiscore.setText("stefan 1.");
-        hiscore.setFont(new Font("Arial", Font.BOLD,29));
-        hiscore.setForeground(Color.white);
-        test.setLayout(new BorderLayout(1, 1));
-        hiscore.setOpaque(true);
-        test.add(hiscore);*/
-
         Controller controller = new Controller();
     }
 }
